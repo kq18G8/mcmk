5222d222394c61548722c4cd7bf4e442e8d020bd
HowarDu <37899657+HowarDu@users.noreply.github.com>
2018-05-03 19:28:40 +0800

    Add files via upload

---
+++ VODKAMYFIEND/house.py
@@ -0,0 +1,21 @@
+import mcpi.minecraft as minecraft
+import mcpi.block as block
+mc=minecraft.Minecraft.create()
+pos=mc.player.getTilePos()
+x=pos.x+2
+y=pos.y
+z=pos.z
+SIZE=20
+def house():
+    midx=x+SIZE/2
+    midy=y+SIZE/2
+    mc.setBlocks(x,y,z+SIZE,y+SIZE,z+SIZE,block.COBBLESTONE.id)
+    mc.setBlocks(x+1,y,z+1,x+SIZE-2,y+SIZE-1,z+SIZE-2,block.AIR.id)
+    mc.setBlocks(midx-1,y,z,midx+1,y+3,z,block.AIR.id)
+    mc.setBlocks(x+3,y+SIZE-3,z,midx-3,midy+3,z,block.GLASS.id)
+    mc.setBlocks(midx+3,y+SIZE-3,z,x+SIZE-3,midy+3,z,block.GLASS.id)
+    mc.setBlocks(x,y+SIZE,z,x+SIZE,y+SIZE,z+SIZE,block.WOOD.id)
+    mc.setBlocks(x+1,y-1,z+1,x+SIZE-2,y-1,z+SIZE-2,block.WOOL.id)
+for xx in range(5):
+    house()
+    x=pos.x+xx*SIZE+5
