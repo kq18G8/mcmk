d578632f702bb36539b07fabbf4e14ca016c90c8
Ryan1234554321 <37899676+Ryan1234554321@users.noreply.github.com>
2018-06-07 18:41:09 +0800

    Add files via upload

---
+++ lalala/lizi.py
@@ -0,0 +1,33 @@
+#coding=utf-8
+
+import mcpi.minecraft as minecraft
+import mcpi.block as block
+
+mc=minecraft.Minecraft.create()
+pos=mc.player.getTilePos()
+
+f=open("mc.txt",'r')
+nameList=f.readlines()
+xCounter=0
+for line in nameList:
+	digitalList=line.split(",")
+	zCounter=0
+	for digitalSingle in digitalList:
+		if digitalSingle=='1':
+			mc.setBlock(pos.x+xCounter,pos.y-1,pos.z-zCounter,block.DIAMOND_BLOCK.id)
+		else:
+			mc.setBlock(pos.x+xCounter,pos.y-1,pos.z-zCounter,block.WOOD.id)
+		zCounter+=1
+	xCounter+=1
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
