be8632338c1839325bd8de3ab9c7f9d5c9eb793c
dasdsadasdsad <37899643+dasdsadasdsad@users.noreply.github.com>
2018-09-27 19:37:18 +0800

    Update roooooooof.py

--- HoneySelectx64/roooooooof.py
+++ HoneySelectx64/roooooooof.py
@@ -1,1 +1,19 @@
 print("Slave, get your dignity back here!")
+print("Slave, get your dignity back here!")
+f=open("roof.csv","r")
+data=f.read()
+
+row233=data.split("\n")
+
+we=row233[2]
+
+us=row233[2].split(",")
+
+
+roof_data=[]
+
+for row in row233:
+    roof_data.append(us[3])
+
+print(roof_data[2])
+    
