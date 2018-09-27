025270f825b6ec742bdd76acf5ea1c64c9a25e1d
DrDicou <37899637+DrDicou@users.noreply.github.com>
2018-09-27 19:29:40 +0800

    roof finish
    I feel blyat.

--- ZhangZhe/roof.py
+++ ZhangZhe/roof.py
@@ -1,1 +1,10 @@
 print("Отправить тебя в гулаг")
+f = open("roof.csv","r")
+data = f.read()
+rows = data.split('\n')
+print(rows[2:3])
+final_data = []
+for row in rows:
+    split_list = row.split(',')
+    final_data.append(split_list)
+print(final_data[2][3])
